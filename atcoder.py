d1127d21e219db61adcd82563cf73e4b3a5a3d74
Ashigaru <44052955+Ashigaru012@users.noreply.github.com>
2019-04-23 17:53:33 +0900

    Add files via upload

---
+++ atcoder.py
@@ -0,0 +1,25 @@
+def f(s):
+    cnt = 1
+    st = set()
+
+    st.add(s)
+
+    while True:
+        cnt += 1
+
+        if s % 2 == 0:
+            tmp = s / 2
+        else:
+            tmp = 3 * s + 1
+
+        if tmp in st:
+            break
+
+        st.add(tmp)
+        s = tmp
+    return cnt
+
+
+s = int(input())
+ans = f(s)
+print(ans)
